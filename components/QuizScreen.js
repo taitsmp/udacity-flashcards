af70df865b185a77d5be4fb50ccbe5b41a37491f
Tait Larson <tait@stylemepretty.com>
2017-11-15 11:00:52 -0500

    Added in most of PanResponder code.

--- components/QuizScreen.js
+++ components/QuizScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { View, Animated, Text, StyleSheet } from 'react-native'
+import { View, Animated, Text, StyleSheet, PanResponder, Dimensions } from 'react-native'
 import { connect } from 'react-redux'
 import { fetchDecks } from '../utils/api'
 import * as Utils from '../utils/utils'
@@ -7,12 +7,62 @@ import PropTypes from 'prop-types'
 import CardView from './CardView'
 import ScoreCard from './ScoreCard'
 
+const SCREEN_WIDTH = Dimensions.get('window').width
+const SWIPE_DURATION = 250
+const SWIPE_THRESHOLD = SCREEN_WIDTH * .25
+
 class QuizScreen extends Component {
   state = {
     cardIndex: 0,
     correct: 0
   }
+  forceSwipe(direction) {
+    const x_dist = direction == 'right' ? SCREEN_WIDTH : -SCREEN_WIDTH
+    Animated.timing(this._position, {
+      duration: SWIPE_DURATION,
+      toValue: {x: x_dist, y: 0}
+    }).start(() => this.onSwipeComplete(direction))
 
+  }
+
+  onSwipeComplete(direction) {
+    //call handleGrade
+    //handleGrade will update the state correctly. 
+    //maybe don't need this function?
+    this._postition.setValue({x:0, y:0}) //do this in handleGrade?
+  }
+
+  //style is used to tell the Animated View where it moves to.
+  //LEFT OFF HERE: set the rotation and grab elements from position.getLayout.  
+  getCardStyle() {
+    return {
+      //this._position.getLayout()
+      
+    }
+  }
+
+  componentWillMount() {
+    let position = Animated.ValueXY() // starts at 0,0?
+    let panResponder = PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderGrant: (evt, gestureState) => {  
+        // The gesture has started. Show visual feedback so the user knows
+        // what is happening!
+        // gestureState.d{x,y} will be set to zero now
+       },
+      onPanResponderMove: (event, gesture) => {
+        position.setValue({x: gesture.dx, y: gesture.dy })
+      },
+      onPanResponderRelease: (event, gesture) => {
+        //detect change then forceSwipe() 
+      }
+
+  
+    })
+    this._position = position  //the position of the movable card.  Gets applied to different cards (depends which is on top)
+    this._panResponder = panResponder
+    
+  }  
   handleGrade = answeredCorrectly => {
     let { correct, cardIndex } = this.state
     cardIndex++
@@ -29,7 +79,6 @@ class QuizScreen extends Component {
     return <ScoreCard deck={deck} correct={correct} deckIndex={deckIndex} navigation={navigation} />
   }
 
-  //LEFT OFF HERE.  Rendering all cards that have not been looked at.  Need to hook up PanResponsder.
   render() {
     const { cardIndex, correct } = this.state
     const { deck, deckIndex, navigation } = this.props
@@ -41,7 +90,10 @@ class QuizScreen extends Component {
 
       if (i === cardIndex) {
         return (
-          <Animated.View style={[styles.cardContainer, { zIndex: 99 }]}>
+          <Animated.View
+            style={[this.getCardStyle(), styles.cardContainer, { zIndex: 99 }]}
+            {...this._panResponder.panHandlers}
+          >
             <CardView deck={deck} cardIndex={cardIndex} handleGrade={this.handleGrade} />
           </Animated.View>
         )
